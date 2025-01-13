b6886b0adc34b24d9da338fba9ab807614231de7
Ahmed Mustafa <107986169+ahmedmustafam@users.noreply.github.com>
2025-01-13 21:06:12 +0400

    Write logic to dynamically size squares
    Squares were the same size before, added math
    rounding function to resize them according to num
    of squares.

--- scripts.js
+++ scripts.js
@@ -1,46 +1,51 @@
-const container = document.getElementById("cont");
-
-function createSquares(num) {
-    for (let i = 0; i < num; i++) {
-        container.innerHTML += '<div class="box"></div>'
-    }
-}
-
-createSquares(256);
-
-
-const boxes = document.querySelectorAll(".box")
-
-boxes.forEach(box => {
-    box.style.backgroundColor = "lightgray";
-})
-
-let isMouseClicked = false;
-
-document.addEventListener("mousedown", () => {
-    isMouseClicked = true;
-})
-
-document.addEventListener("mouseup", () => {
-    isMouseClicked = false;
-})
-
-boxes.forEach(box => {
-    box.addEventListener("mouseover", () => {
-        if (isMouseClicked) {
-            box.style.backgroundColor = "black";
-        }
-    });
-});
-
 const boxSelectorButton = document.createElement("button");
 boxSelectorButton.textContent = "Click Me to begin!";
 document.body.insertBefore(boxSelectorButton, document.body.firstChild);
 
-boxSelectorButton.addEventListener("click", () => {
-    let squaresNumber = parseInt(prompt("How many squares per side for the new grid?", ''));
-    container.innerHTML = '';
-    console.log(squaresNumber)
-    createSquares(squaresNumber**2); //fix this
+const container = document.getElementById("cont");
 
+function createSquares(num) {
+    container.innerHTML = '';
+    const columns = Math.ceil(Math.sqrt(num));
+    const boxSize = 100/columns;
+    for (let i = 0; i < num; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.flex = `1 1 calc(${boxSize}%)`;
+        container.appendChild(box);
+    }
+
+    const boxes = document.querySelectorAll(".box")
+
+    boxes.forEach(box => {
+        box.style.backgroundColor = "lightgray";
+    })
+// Making the painting work on mouse hold
+    let isMouseClicked = false;
+    document.addEventListener("mousedown", () => {
+        isMouseClicked = true;
+    })
+    document.addEventListener("mouseup", () => {
+        isMouseClicked = false;
+    })
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            if (isMouseClicked) {
+                box.style.backgroundColor = "black";
+            }
+        });
+    });
+}
+
+createSquares(256);
+
+boxSelectorButton.addEventListener("click", () => {
+    let numSquares = parseInt(prompt("Select the number of squares on each side from 1-100", ''));
+    createSquares(numSquares**2);
 })
+
+
+
+
+
+
